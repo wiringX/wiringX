f3f2b331aaa30f6b5417143daeb7b643b8fc661e
CurlyMoo <curlymoo1@gmail.com>
2014-10-09 22:15:14 +0200

    Added interrupt example

---
+++ examples/interrupt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "wiringX.h"
+
+void *interrupt(void *param) {
+	while(1) {
+		if(waitForInterrupt(1, 1000) > 0) {
+			printf("interrupt\n");
+		} else {
+			printf("timeout\n");
+		}
+	}
+}
+
+int main(void) {
+	pthread_t pth;
+
+	wiringXSetup();
+	pthread_create(&pth, NULL, interrupt, NULL);
+
+	pinMode(0, OUTPUT);
+	wiringXISR(1, INT_EDGE_BOTH);
+	while(1) {
+		digitalWrite(0, HIGH);
+		sleep(1);
+		digitalWrite(0, LOW);
+		sleep(2);
+	}
+}
