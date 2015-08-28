7d3d0545502fcf097765d525800dd03e86268e86
vitalogy <vitalogy_github@milaw.biz>
2015-08-28 17:15:57 +0200

    examples: GPIO's must manual set by the user + error handling

--- examples/blink.c
+++ examples/blink.c
@@ -4,32 +4,46 @@
 
 #include "wiringX.h"
 
+char *usage = "Usage: blink GPIO\n"
+			"       GPIO is the GPIO to write\n"
+			"Example: blink 10";
+
 int main(int argc, char *argv[]) {
-	int pin = 0;
+	int gpio;
+	int i, n, value;
+	char ch;
 
-	wiringXSetup();
-
-	if (argc == 1) {
-		printf("blink: Using default GPIO 0\n");
-	} else {
-		if (argc == 2) {
-			pin = atoi(argv[1]);
-			if(wiringXValidGPIO(pin) != 0) {
-				printf("blink: Invalid GPIO %d\n", pin);
-				return -1;
-			}
-			printf("blink: Using GPIO %d\n", pin);
-		} else {
-			printf("blink: Too many arguments\n");
+	// first, check for a valid, numeric argument
+	// found at http://stackoverflow.com/questions/17292545
+	for(i=1; i<argc; i++) {
+		n = sscanf(argv[i], "%d%c", &value, &ch);
+		if (n != 1) {
+			printf("%s: Invalid GPIO %s\n", argv[0], argv[i]);
 			return -1;
 		}
 	}
 
-	pinMode(pin, OUTPUT);
+	// expect 1 argument for the programm blink ('blink gpio')
+	// these are argv[0-1], so argc = 2
+	if(argc != 2) {
+		printf("%s\n", usage);
+		return -1;
+	}
+
+	wiringXSetup();
+
+	gpio = atoi(argv[1]);
+
+	if(wiringXValidGPIO(gpio) != 0) {
+		printf("%s: Invalid GPIO %d\n", argv[0], gpio);
+		return -1;
+	}
+
+	pinMode(gpio, OUTPUT);
 	while(1) {
-		digitalWrite(pin, HIGH);
+		digitalWrite(gpio, HIGH);
 		sleep(1);
-		digitalWrite(pin, LOW);
+		digitalWrite(gpio, LOW);
 		sleep(1);
 	}
 }
--- examples/read.c
+++ examples/read.c
@@ -4,19 +4,61 @@
 
 #include "wiringX.h"
 
-int main(void) {
+char *usage = "Usage: read GPIO GPIO\n"
+			"       first GPIO to write to = output\n"
+			"       second GPIO to read from = input\n"
+			"Example: read 4 23";
+
+int main(int argc, char *argv[]) {
+	int gpio_out, gpio_in;
+	int i, n, value;
+	char ch;
+
+	// first, check for valid, numeric arguments
+	// found at http://stackoverflow.com/questions/17292545
+	for(i=1; i<argc; i++) {
+		n = sscanf(argv[i], "%d%c", &value, &ch);
+		if (n != 1) {
+			printf("%s: Invalid GPIO %s\n", argv[0], argv[i]);
+			return -1;
+		}
+	}
+
+	// expect 2 arguments for the programm read ('read gpio_out gpio_in')
+	// these are argv[0-2], so argc = 3
+	if(argc != 3) {
+		printf("%s\n", usage);
+		return -1;
+	}
+
 	wiringXSetup();
 
-	pinMode(0, OUTPUT);
-	pinMode(1, INPUT);
+	gpio_out = atoi(argv[1]);
+	gpio_in = atoi(argv[2]);
+	if(gpio_out == gpio_in) {
+		printf("%s: GPIO for output and input should not be the same\n", argv[0]);
+		return -1;
+	}
+	if(wiringXValidGPIO(gpio_out) != 0) {
+		printf("%s: Invalid GPIO %d for output\n", argv[0], gpio_out);
+		return -1;
+	}
+	if(wiringXValidGPIO(gpio_in) != 0) {
+		printf("%s: Invalid GPIO %d for input\n", argv[0], gpio_in);
+		return -1;
+	}
+
+	pinMode(gpio_out, OUTPUT);
+	pinMode(gpio_in, INPUT);
+
 	while(1) {
-		printf("Writing to pin 0: High\n");
-		digitalWrite(0, HIGH);
-		printf("Reading from pin 1: %d\n", digitalRead(1));
+		printf("Writing to GPIO %d: High\n", gpio_out);
+		digitalWrite(gpio_out, HIGH);
+		printf("Reading from GPIO %d: %d\n", gpio_in, digitalRead(gpio_in));
 		sleep(1);
-		printf("Writing to pin 0: Low\n");
-		digitalWrite(0, LOW);
-		printf("Reading from pin 1: %d\n", digitalRead(1));
+		printf("Writing to GPIO %d: Low\n", gpio_out);
+		digitalWrite(gpio_out, LOW);
+		printf("Reading from GPIO %d: %d\n", gpio_in, digitalRead(gpio_in));
 		sleep(1);
 	}
 }
