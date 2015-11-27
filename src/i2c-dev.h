6b0a90eefc64df1d675e068fce47126dd744f8fa
CurlyMoo <curlymoo1@gmail.com>
2015-11-27 11:15:18 +0100

    Proper i2c-dev definitions

--- src/i2c-dev.h
+++ src/i2c-dev.h
@@ -14,12 +14,12 @@
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 
-inline __s32 i2c_smbus_access(int fd, char rw, int cmd, int size, union i2c_smbus_data *data);
-inline __s32 i2c_smbus_read_byte(int fd);
-inline __s32 i2c_smbus_write_byte(int fd, int value);
-inline __s32 i2c_smbus_read_byte_data(int fd, int cmd);
-inline __s32 i2c_smbus_write_byte_data(int fd, int cmd, int value);
-inline __s32 i2c_smbus_read_word_data(int fd, int cmd);
-inline __s32 i2c_smbus_write_word_data(int fd, int cmd, __u16 value);
+extern inline __s32 i2c_smbus_access(int fd, char rw, int cmd, int size, union i2c_smbus_data *data);
+extern inline __s32 i2c_smbus_read_byte(int fd);
+extern inline __s32 i2c_smbus_write_byte(int fd, int value);
+extern inline __s32 i2c_smbus_read_byte_data(int fd, int cmd);
+extern inline __s32 i2c_smbus_write_byte_data(int fd, int cmd, int value);
+extern inline __s32 i2c_smbus_read_word_data(int fd, int cmd);
+extern inline __s32 i2c_smbus_write_word_data(int fd, int cmd, __u16 value);
 
 #endif
