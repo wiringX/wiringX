e1b72456ab59fa68a2338cbed832a02a8a2ac735
Falk <fne@alice-dsl.net>
2019-01-24 22:51:32 +0100

    adjustment in copyright line

--- src/soc/allwinner/a20.c
+++ src/soc/allwinner/a20.c
@@ -1,5 +1,6 @@
 /*
-	Copyright (c) 2018
+	Copyright (c) 2016 CurlyMo <curlymoo1@gmail.com>
+	Copyright (c) 2018 Falk Nedwal
 
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
