26887eaa5b79b93978d5a13c677cef4c24676716
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-11-22 16:16:50 +0800

    Extract common features from rk3399/rock4
    This commit prepares WiringX to support additional Rockchip SoC.
    
    Signed-off-by: Nascs Fang <nascs@radxa.com>
    Co-authored-by: ZHANG Yuntian <yt@radxa.com>

--- src/soc/rockchip/rk3399.h
+++ src/soc/rockchip/rk3399.h
@@ -1,17 +1,17 @@
 /*
-  Copyright (c) 2022 Radxa Ltd.
-  Author: Nascs <nascs@radxa.com>
+	Copyright (c) 2022-2023 Radxa Ltd.
+	Author: Nascs <nascs@radxa.com>
+					ZHANG Yuntian <yt@radxa.com>
 
-  This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #ifndef __WIRINGX_ROCKCHIP3399_H
 #define __WIRINGX_ROCKCHIP3399_H
 
-#include "../soc.h"
-#include "../../wiringx.h"
+#include "common.h"
 
 extern struct soc_t *rk3399;
 
